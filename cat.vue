8a44da15f3057602809e36f6a7eb0c91a3eb6f1d
QiShaoXuan <474021214@qq.com>
2019-08-14 10:30:02 +0800

    remove scss to css

--- cat.vue
+++ cat.vue
@@ -1,11 +1,11 @@
-<style scoped lang="scss">
-.cat-container {
-  position: fixed;
-  right: 50px;
-  bottom: 100px;
-  color: #00adb5;
-
-  #vuepress-cat {
+<style scoped>
+  .cat-container {
+    position: fixed;
+    right: 50px;
+    bottom: 100px;
+    color: #00adb5;
+  }
+  .cat-container #vuepress-cat {
     position: fixed;
     opacity: 0.9;
     right: 0px;
@@ -13,7 +13,6 @@
     z-index: 99999;
     pointer-events: none;
   }
-}
 </style>
 
 <template>
@@ -28,63 +27,64 @@
 </template>
 
 <script>
-import live2dJSString from "./live2d";
-export default {
-  name: "cat",
-  data() {
-    return {
-      isLoaded: true,
-      model: {
-        blackCat:
-          "https://cdn.jsdelivr.net/gh/QiShaoXuan/live2DModel@1.0.0/live2d-widget-model-hijiki/assets/hijiki.model.json",
-        whiteCat:
-          "https://cdn.jsdelivr.net/gh/QiShaoXuan/live2DModel@1.0.0/live2d-widget-model-tororo/assets/tororo.model.json"
-      },
-      style: {
-        width: 280,
-        height: 250
-      }
-    };
-  },
-  mounted() {
-    this.initCat();
+  import live2dJSString from "./live2d";
 
-    this.$router.afterEach((to, from) => {
-      if (to.path !== from.path) {
-        this.initCat();
-      }
-    });
-  },
-  methods: {
-    initCat() {
-      const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-        ? true
-        : false;
-      if (isMobile) {
-        this.isLoaded = false;
-        return console.log("mobile do not load model");
-      }
-
-      if (!window.loadlive2d) {
-        const script = document.createElement("script");
-        script.innerHTML = live2dJSString;
-        document.body.appendChild(script);
-      }
-
-      this.style = {
-        width: (150 / 1424) * document.body.clientWidth,
-        height: ((150 / 1424) * document.body.clientWidth) / 0.8
+  export default {
+    name: "cat",
+    data() {
+      return {
+        isLoaded: true,
+        model: {
+          blackCat:
+            "https://cdn.jsdelivr.net/gh/QiShaoXuan/live2DModel@1.0.0/live2d-widget-model-hijiki/assets/hijiki.model.json",
+          whiteCat:
+            "https://cdn.jsdelivr.net/gh/QiShaoXuan/live2DModel@1.0.0/live2d-widget-model-tororo/assets/tororo.model.json"
+        },
+        style: {
+          width: 280,
+          height: 250
+        }
       };
+    },
+    mounted() {
+      this.initCat();
 
-      setTimeout(() => {
-        window.loadlive2d(
-          "vuepress-cat",
-          Math.random() > 0.5 ? this.model.blackCat : this.model.whiteCat
-        );
+      this.$router.afterEach((to, from) => {
+        if (to.path !== from.path) {
+          this.initCat();
+        }
       });
+    },
+    methods: {
+      initCat() {
+        const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+          ? true
+          : false;
+        if (isMobile) {
+          this.isLoaded = false;
+          return console.log("mobile do not load model");
+        }
+
+        if (!window.loadlive2d) {
+          const script = document.createElement("script");
+          script.innerHTML = live2dJSString;
+          document.body.appendChild(script);
+        }
+
+        this.style = {
+          width: (150 / 1424) * document.body.clientWidth,
+          height: ((150 / 1424) * document.body.clientWidth) / 0.8
+        };
+
+        setTimeout(() => {
+          window.loadlive2d(
+            "vuepress-cat",
+            Math.random() > 0.5 ? this.model.blackCat : this.model.whiteCat
+          );
+        });
+      }
     }
-  }
-};
+  };
 </script>
